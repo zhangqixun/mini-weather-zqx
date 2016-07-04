de837c0d557d33f41a30fca92548570a91774ef0
zhangqixun <zhangqixun@Mac-Pro.local>
2016-07-04 20:10:42 +0800

    add NetUtil

--- app/src/main/java/cn/edu/pku/zhangqixun/miniweather/MainActivity.java
+++ app/src/main/java/cn/edu/pku/zhangqixun/miniweather/MainActivity.java
@@ -2,6 +2,10 @@ package cn.edu.pku.zhangqixun.miniweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import cn.edu.pku.zhangqixun.util.NetUtil;
 
 /**
  * Created by zhangqixun on 16/7/4.
@@ -12,5 +16,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d("myWeather", "网络OK");
+            Toast.makeText(MainActivity.this,"网络OK！", Toast.LENGTH_LONG).show();
+        }else
+        {
+            Log.d("myWeather", "网络挂了");
+            Toast.makeText(MainActivity.this,"网络挂了！", Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ app/src/main/java/cn/edu/pku/zhangqixun/util/NetUtil.java
@@ -0,0 +1,34 @@
+package cn.edu.pku.zhangqixun.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by zhangqixun on 16/7/4.
+ */
+public class NetUtil {
+    public static final int NETWORN_NONE = 0;
+    public static final int NETWORN_WIFI = 1;
+    public static final int NETWORN_MOBILE = 2;
+
+    public static int getNetworkState(Context context) {
+        ConnectivityManager connManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
+        if (networkInfo == null) {
+            return NETWORN_NONE;
+        }
+
+        int nType = networkInfo.getType();
+        if (nType == ConnectivityManager.TYPE_MOBILE) {
+            return NETWORN_MOBILE;
+        } else if (nType == ConnectivityManager.TYPE_WIFI) {
+            return NETWORN_WIFI;
+        }
+        return NETWORN_NONE;
+    }
+
+
+}
